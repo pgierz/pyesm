890b17cb06d0fd998f277da6ba5badbdd1f09988
Paul Gierz <pgierz@awi.de>
2019-01-22 07:17:56 +0100

    mimimal packaging, still needs to integrate requirements...

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pyesm',
+      version='2.0',
+      description='Python-based Earth System Model Infrastructure Tools',
+      author='D. Barbi, P. Gierz, N. Wieters',
+      author_email='pgierz@awi.de',
+      url='https://github.com/pgierz/pyesm',
+      packages=find_packages(), 
+     )
