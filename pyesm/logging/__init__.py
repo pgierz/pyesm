02800f657fcfbea547b342e8949946358ee66247
Paul Gierz <pgierz@awi.de>
2019-01-22 04:50:49 +0100

    some docs and clarification

--- pyesm/logging/__init__.py
+++ pyesm/logging/__init__.py
@@ -9,7 +9,6 @@ TODO:
 
 """
 
-
 import inspect
 import logging
 import os
@@ -21,18 +20,29 @@ THIS_PID = str(os.getpid())
 PARENT_PID = str(os.getppid())
 
 
-
 def set_logging_this_module(log_dir=None, log_tag=None):
-    frm = inspect.stack()[1]
-    mod = inspect.getmodule(frm[0])
+    """ Sets logging for whatever calls this function to ModuleName_<log_tag>_<PID>.log
+    
+    Parameters
+    ----------
+    log_dir : str, optional
+        The directory where the log file should be written
+    log_tag : str, optional
+        Default is None. This can be a string used to identify the logfile
+
+    Returns
+    -------
+    logger : logging.Logger
+        A logger object is returned with the specified filename.
+    """
+    file_name_of_calling_function = inspect.stack()[1]
+    mod = inspect.getmodule(file_name_of_calling_function[0])
     logger = logging.getLogger(mod.__name__)
     logger.setLevel(logging.INFO)
 
-    if log_tag:
-        log_file_parts = [mod.__name__, log_tag, THIS_PID]
-    else:
-        log_file_parts = [mod.__name__, THIS_PID]
+    log_file_parts = [x for x in [mod.__name__, log_tag, THIS_PID] if x !=None]
     log_file = "_".join(log_file_parts)+".log"
+
     if log_dir:
         log_file = "/".join([log_dir, log_file])
     file_handler = logging.FileHandler(log_file)
@@ -42,6 +52,7 @@ def set_logging_this_module(log_dir=None, log_tag=None):
 
     logger.addHandler(file_handler)
     return logger
+
 
 def set_logging_main_output(expid=None, jobtype=None, date=None):
     """ Sets logging to a file for the 'main' (info) output """
